a40ff203cc39c42352885ecbaf5f24bea6944b3b
carlos roberto <carlosrobertounopar01@hotmail.com>
2022-03-05 16:05:12 -0300

    Testes finalizados

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,26 +1,27 @@
 defmodule FizzBuzz do
-  def build (file_name) do
+  def build(file_name) do
     file_name
     |> File.read()
     |> handle_file_read()
   end
 
   defp handle_file_read({:ok, result}) do
-  {:ok,  result
-   |> String.split(",")
-   |> Enum.map(&convert_and_evaluate_numbers/1)}
-  end 
+    {:ok,
+     result
+     |> String.split(",")
+     |> Enum.map(&convert_and_evaluate_numbers/1)}
+  end
 
-  defp handle_file_read({:error,reason}), do: {:error, "Erro reading file #{reason}"} 
+  defp handle_file_read({:error, reason}), do: {:error, "Erro reading file #{reason}"}
 
   defp convert_and_evaluate_numbers(number) do
-  number
+    number
     |> String.to_integer()
     |> evaluate_numbers()
   end
 
-  defp evaluate_numbers(number) when rem(number,3) == 0 and rem(number,5) == 0, do: :FizzBuzz
-  defp evaluate_numbers(number) when rem(number,3) == 0, do: :Fizz
-  defp evaluate_numbers(number) when rem(number,5) == 0, do: :Buzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :FizzBuzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :Fizz
+  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :Buzz
   defp evaluate_numbers(number), do: number
 end
--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -1,8 +1,21 @@
 defmodule FizzBuzzTest do
-#   use ExUnit.Case
-#  doctest FizzBuzz
+  use ExUnit.Case
 
-#  test "greets the world" do
-#  assert FizzBuzz.hello() == :world
-#  end
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      response = FizzBuzz.build("number.txt")
+
+      expected_response = {:ok, [1, 2, :Fizz, 4, :Buzz, :Buzz, :FizzBuzz, :Buzz]}
+
+      assert response == expected_response
+    end
+
+    test "when a valid file is provided, returns an error" do
+      response = FizzBuzz.build("notexist.txt")
+
+      expected_response = {:error, "Erro reading file enoent"}
+
+      assert response == expected_response
+    end
+  end
 end
